a5f607c57bb887cec50eb3f7198460f7c712fd02
SimonGodefroid <simon.godefroid@gmail.com>
2017-06-20 23:39:10 +0200

    wording

--- src/components/ResponsiveCarousel.js
+++ src/components/ResponsiveCarousel.js
@@ -1,91 +1,92 @@
 import React, { Component } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.css";
-import Slides from './Slides';
+import Slides from "./Slides";
 
 console.log("window", window.innerWidth);
 
 let imagesMoveet = [
   {
     url: "./assets/img/moveet_landing_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Explorez les films par genre'
+    alt: "moveet_landing",
+    legend: "Explorez les films par genre"
   },
   {
     url: "./assets/img/moveet_results_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Découvrez vos prochains coups de coeur'
+    alt: "moveet_landing",
+    legend: "Découvrez vos prochains coups de coeur"
   },
   {
     url: "./assets/img/moveet_movie_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Accèdez aux informations sur le film'
+    alt: "moveet_landing",
+    legend: "Accèdez aux informations sur le film"
   },
   {
     url: "./assets/img/moveet_buddyfinder_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Trouvez un buddy passionné pour aller avec vous'
+    alt: "moveet_landing",
+    legend: "Trouvez un buddy passionné pour aller avec vous"
   },
   {
     url: "./assets/img/moveet_buddyprofile_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Comparez vos pipelines de films'
+    alt: "moveet_landing",
+    legend: "Comparez vos pipelines de films"
   },
   {
     url: "./assets/img/moveet_showtimes_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Trouvez les horaires pour le film'
+    alt: "moveet_landing",
+    legend: "Trouvez les séances pour le film"
   },
   {
     url: "./assets/img/moveet_buddieslist_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Parcourez la liste des buddies et des matches'
+    alt: "moveet_landing",
+    legend: "Parcourez la liste des buddies et des matches"
   },
   {
     url: "./assets/img/1_moveet_landing.jpg",
-    alt: 'moveet_landing',
-    legend: 'Chattez avec vos buddies'
+    alt: "moveet_landing",
+    legend: "Partagez votre passion en chattant avec vos buddies"
   },
   {
     url: "./assets/img/moveet_swiper_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Swippez pour constituer votre pipeline de films'
+    alt: "moveet_landing",
+    legend: "Swippez pour constituer votre pipeline de films"
   },
   {
     url: "./assets/img/moveet_swiper_yes_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'À droite pour ajouter le film à votre pipeline'
+    alt: "moveet_landing",
+    legend: "À droite pour ajouter le film à votre pipeline"
   },
   {
     url: "./assets/img/moveet_swiper_no_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'À gauche pour le zapper'
+    alt: "moveet_landing",
+    legend: "À gauche pour le zapper"
   },
   {
     url: "./assets/img/moveet_aroundme_theaters_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Dénichez les cinémas autour de vous'
+    alt: "moveet_landing",
+    legend: "Dénichez les cinémas autour de vous"
   },
   {
     url: "./assets/img/1_moveet_landing.jpg",
-    alt: 'moveet_landing',
-    legend: 'Dévoilez les passionnés qui vous entourent'
+    alt: "moveet_landing",
+    legend: "Rencontrez les passionnés autour de vous"
   },
   {
     url: "./assets/img/moveet_profile_page_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Visionnez votre profil'
+    alt: "moveet_landing",
+    legend: "Visionnez votre profil"
   },
   {
     url: "./assets/img/moveet_profile_page_2_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Consultez votre pipeline depuis celui-ci'
-  }, {
+    alt: "moveet_landing",
+    legend: "Consultez votre pipeline depuis celui-ci"
+  },
+  {
     url: "./assets/img/moveet_edit_profile_2_lowq.jpg",
-    alt: 'moveet_landing',
-    legend: 'Gérez vos informations personnelles'
+    alt: "moveet_landing",
+    legend: "Gérez vos informations personnelles"
   }
-]
+];
 
 export default class ResponsiveCarousel extends Component {
   constructor(props) {
@@ -96,7 +97,7 @@ export default class ResponsiveCarousel extends Component {
       fontSize: "20px",
       fontColor: "white",
       marginLeft: "auto",
-      padding: "0 auto",
+      padding: "0 auto"
     };
   }
 
@@ -126,27 +127,27 @@ export default class ResponsiveCarousel extends Component {
   }
 
   renderSlides(images) {
-
     const slides = images.map((image, key) => {
       return (
-        < Slides
+        <Slides
           key={key}
           url={image.url}
           alt={image.alt}
           legend={image.legend}
-          fontSize={this.state.fontSize} />
-      )
-    })
-    return slides
+          fontSize={this.state.fontSize}
+        />
+      );
+    });
+    return slides;
   }
 
   render() {
     return (
-      <div style={{ padding: this.state.padding, marginTop: '20px' }}>
-        <div style={{ textAlign: 'center' }}>
-          <p style={{ fontWeight: 700, padding: '20px' }}>MOVEET</p>
+      <div style={{ padding: this.state.padding, marginTop: "20px" }}>
+        <div style={{ textAlign: "center" }}>
+          <p style={{ fontWeight: 700, padding: "20px" }}>MOVEET</p>
         </div>
-        < Carousel
+        <Carousel
           showArrows={false}
           showThumbs={false}
           infiniteLoop
